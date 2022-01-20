829f2987edd0d119524cdad9ecd6e5f68e36c1a1
MidnightOwll <dv92040@gmail.com>
2022-01-20 18:56:56 +0300

    полиморфизм

--- index.php
+++ index.php
@@ -38,21 +38,60 @@ class Person {
     }
 }
 
-$person1 = new Person('Демиденко', 'Виталий', '22');
-$person2 = new Person('Петров', 'Петр', '24');
+$person1 = new Person('Демиденко', 'Виталий', '21');
+$person2 = new Person('Петров', 'Петр', '22');
 echo $person1->hello();
 echo "<br>";
 echo $person2->hello();
 echo "<br>";
 echo "<br>";
 //Наследование
-class Proger extends Person {
+class Worker extends Person {
+    private $inn;
+
+    public function __construct($lastName, $name, $age, $inn){
+        parent::__construct($lastName, $name, $age);
+        $this->inn = $inn;
+    }   
+    function hello(){
+        parent::hello();
+        echo ' и я работник';
+    }
+}
+$worker1 = new Worker('Демиденко', 'Виталий', '22','1111111111');
+$worker2 = new Worker('Петров', 'Петр', '24', '2222222222');
+echo $worker1->hello();
+echo "<br>";
+echo $worker2->hello();
+echo "<br>";
+echo "<br>";
+
+class Proger extends Worker {
     private $prLang;
 
-    public function __construct($lastName, $name, $age, $prLang){
-        parent::__construct($lastName, $name, $age);
+    public function __construct($lastName, $name, $age, $inn, $prLang){
+        parent::__construct($lastName, $name, $age, $inn);
         $this->prLang = $prLang;
     }   
+    function hello(){
+        parent::hello();
+        echo ', а именно программист';
+    }
 }
-$proger1 = new Proger('Демиденко', 'Станистлав', '18', 'PHP');
+
+$proger1 = new Proger('Демиденко', 'Станистлав', '18', '4444444444', 'Java');
 echo $proger1->hello();
+// полиморфизм
+$persons = [$person1, $worker1, $proger1];
+function sayHelloAll($person) {
+     for ($i = 0; $i < count($person); $i++) { 
+         $person[$i]->hello();
+         echo "<br>";
+     }
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+sayHelloAll($persons);
